6e2d37468758a19373b5131c38fbf1bb7107d98a
Giraafje <lenka.erbenova79@gmail.com>
2023-10-26 21:13:22 +0200

    First part done and working as expected.

--- src/components/Emoticon/index.jsx
+++ src/components/Emoticon/index.jsx
@@ -1,14 +1,20 @@
 import './style.css';
+import { useSettings } from '../../setting-context'
+import {eyesData, mouthData} from '../../data'
 
-import eyesImg from '../../img/eyes2.svg';
-import mouthImg from '../../img/mouth2.svg';
+// import eyesImg from '../../img/eyes2.svg';
+// import mouthImg from '../../img/mouth2.svg';
 
 const Emoticon = () => {
+	const value = useSettings();
+
+	const eyes = eyesData.find((eye) => eye.id === value.eyes)
+	const mouth = mouthData.find((mouth) => mouth.id === value.mouth)
 
 	return (
 		<div className='emoticon' style={{ backgroundColor: '#ff2e12' }}>
-			<img className="emoticon__eyes" src={eyesImg} />
-			<img className="emoticon__mouth" src={mouthImg} />
+			<img className="emoticon__eyes" src={eyes.image} />
+			<img className="emoticon__mouth" src={mouth.image} />
 		</div>
 	);
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,20 +1,30 @@
 import { createRoot } from 'react-dom/client';
 import './style.css';
+import { useState } from 'react'
+import { SettingContext } from './setting-context';
 
 import Emoticon from './components/Emoticon';
 import Configurator from './components/Configurator';
 
 const App = () => {
+  const [settings, setSettings] = useState({
+    eyes: 1,
+    mouth: 2,
+    color: 3
+  })
+
   return (
-    <div className="container">
-      <header>
-        <h1>Vytvoř si vlastní emotikon</h1>
-      </header>
-      <main>
-        <Emoticon />
-        <Configurator />
-      </main>
-    </div>
+    <SettingContext.Provider value={{...settings}}>
+      <div className="container">
+        <header>
+          <h1>Vytvoř si vlastní emotikon</h1>
+        </header>
+        <main>
+          <Emoticon />
+          <Configurator />
+        </main>
+      </div>
+    </SettingContext.Provider>
   );
 };
 
